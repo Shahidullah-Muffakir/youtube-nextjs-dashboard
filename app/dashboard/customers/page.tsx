79727d25922472ac4ca83f6d1b2a09e6c5aee39d
Shahidullah Muffakir <shahidmuffakir@gmail.com>
2024-01-03 11:03:03 +0530

    modfied ts config

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+const Page:NextPage  = () => {
+    return (
+        <div>
+
+
+            <p>cusomters page</p>
+        </div>
+    )
+}
+
+export default Page;
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            <p>incvoice page</p>
+        </div>
+    )
+}
